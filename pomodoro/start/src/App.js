8ad7b4be4f333b9d1685e2c773269998b0810cb5
Alvin <huerox@msn.com>
2020-06-25 16:22:12 -0700

    added state variables for minutes, seconds, and title using useState and used padStart to ensure that they are double digits. now working on a startTimer function to begin the timer with an onclick event to the start button.

--- pomodoro/start/src/App.js
+++ pomodoro/start/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+// Pad time function uses the padStart method to ensure that the timer has 2 digits for minutes and seconds.
+function padTime(time) {
+  return time.toString().padStart(2, "0")
+};
+
+
 export default function App() {
+  //state variables used to set title and time left
+const [title, setTitle] = useState('Let the countdown begin!!!')
+const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+function startTimer(){
+
+};
+  //minute and second variables 
+const minutes = padTime(Math.floor(timeLeft / 60));
+const seconds = padTime((timeLeft - minutes * 60));
+
   return (
     <div className="app">
-      <h2>This is my Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
