145bcdca7e7fa42713b1f0dc169a53fd8967fca9
Alvin <huerox@msn.com>
2020-06-26 11:13:39 -0700

    added a stop timer function.

--- pomodoro/start/src/App.js
+++ pomodoro/start/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef} from 'react';
 import './App.css';
 
-// Pad time function uses the padStart method to ensure that the timer has 2 digits for minutes and seconds.
+// Pad time helper function uses the padStart method to ensure that the timer has 2 digits for minutes and seconds.
 function padTime(time) {
   return time.toString().padStart(2, "0")
 };
@@ -11,18 +11,27 @@ export default function App() {
   //state variables used to set title and time left
 const [title, setTitle] = useState('Let the countdown begin!!!')
 const [timeLeft, setTimeLeft] = useState(10);
+const intervalRef = useRef(null);
 
 function startTimer(){
-  setInterval(() => {
+  setTitle(`You're doing great!`)
+ intervalRef.current = setInterval(() => {
     setTimeLeft(timeLeft => {
 
       if(timeLeft >= 1) {
         return timeLeft - 1
       }
+      //reset timer here
       return 0;
     })
   }, 1000)
 };
+  //stop timer function
+function stopTimer(){
+  clearInterval(intervalRef.current);
+  setTitle('Keep it up!');
+}
+
   //minute and second variables 
 const minutes = padTime(Math.floor(timeLeft / 60));
 const seconds = padTime((timeLeft - minutes * 60));
@@ -39,7 +48,7 @@ const seconds = padTime((timeLeft - minutes * 60));
 
       <div className="buttons">
         <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
+        <button onClick={stopTimer}>Stop</button>
         <button>Reset</button>
       </div>
     </div>
