4fdc1033d5f71738f2fcc41345dc1acfa74f38ee
Alvin <huerox@msn.com>
2020-06-29 12:47:39 -0700

    created a markdown editor using React markdown.

--- markdown-editor/start/src/App.js
+++ markdown-editor/start/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState }from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# sup');
+
+  function handleChange(e){
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea 
+        onChange={handleChange} 
+        value={markdown}
+      />
+      
+  {/* <div className="preview" dangerouslySetInnerHTML={{ __html: marked(markdown) }} /> */}
+    <div className="preview"> <ReactMarkdown source={markdown}/></div>
     </div>
   );
 }
