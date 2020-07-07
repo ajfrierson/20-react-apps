2ef0c177240d5bdac36f1bc73e8881c365e0d4a6
Alvin <huerox@msn.com>
2020-07-07 14:08:10 -0700

    added tab component and completed the highligting for on mouse move.

--- highlight-tabs/start/src/App.js
+++ highlight-tabs/start/src/App.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Tab({ children }){
+
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  function moveHighLight(e) {
+   //update highlight style to move the highlight
+   setHighlightStyle({ left: e.nativeEvent.layerX -150 });
+  }
+
+  function hideHightlight(e) {
+   setHighlightStyle({ left: e.nativeEvent.layerX -150, opacity: 0 });
+  }
+
+  return (
+    <div className="tab" onMouseOut={hideHightlight} onMouseMove={moveHighLight}>
+      <div className="highlight" style={highlightStyle} />
+      {children}
+    </div>
+  );
+}
+
 function App() {
 
-   const [highlightStyle, setHighlightStyle] = useState({ left: 0})
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <div className="highlight" style={{left: -100}} />
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
+          <Tab>
+              <a>Home</a>
+          </Tab>
+          <Tab>
+              <a>About</a>
+          </Tab>
+          <Tab>
+              <a>Contact</a>
+          </Tab>
         </div>
 
         <div className="viewport">Pages Go Here</div>
