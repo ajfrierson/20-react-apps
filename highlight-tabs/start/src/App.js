e212b353b19e37fe0c10537e3ce69ff68c5c7c08
Alvin <huerox@msn.com>
2020-07-08 11:08:37 -0700

    refactored code to include a header and routes component.

--- highlight-tabs/start/src/App.js
+++ highlight-tabs/start/src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Routes from './Routes';
 import './App.css';
-import Tab from './components/Tab';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import Features from './pages/Features';
-
 
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="browser">
-          <div className="tabs">
-            <Tab>
-                <NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink>
-            </Tab>
-            <Tab>
-                <NavLink to='/about' activeClassName='is-active'>About</NavLink>
-            </Tab>
-            <Tab>
-                <NavLink to='features' activeClassName='is-active'>Features</NavLink>
-            </Tab>
-          </div>
+          <Header />
 
-          <div className="viewport">   
-            <Switch>
-              <Route path="/" exact={true}>
-                    <Home />
-              </Route>
-              <Route path="/about">
-                    <About />
-              </Route>
-              <Route path="/features">
-                    <Features />
-              </Route>
-            </Switch>
+          <div className="viewport">
+            <Routes />
           </div>
         </div>
       </div>
